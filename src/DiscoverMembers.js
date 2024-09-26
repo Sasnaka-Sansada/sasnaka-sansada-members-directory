cdd0dea8d5cce057609fb2e46143daec87374b8f
realsandaru <sandaruherath.snh@gmail.com>
2024-09-26 18:46:23 +0530

    Discover Members
    This is an update for the Discover Members and Card components.

---
+++ src/DiscoverMembers.js
@@ -0,0 +1,54 @@
+import Account from './Card';
+
+
+function DiscoverMembers(){
+    return (
+        <div>
+            <div className='flex flex-row justify-around flex-wrap w-100'>
+              <div>
+                <Account label1="A Perera"/>
+              </div>
+              <div>
+                <Account label1="B Perera"/>
+              </div>
+              <div>
+                <Account label1="C Perera"/>
+              </div>
+
+              <div>
+                <Account label1="D Perera"/>
+              </div>
+              <div>
+                <Account label1="E Perera"/>
+              </div>
+              <div>
+                <Account label1="F Perera"/>
+              </div>
+              <div>
+                <Account label1="G Perera"/>
+              </div>
+              <div>
+                <Account label1="H Perera"/>
+              </div>
+              <div>
+                <Account label1="I Perera"/>
+              </div>
+              <div>
+                <Account label1="G Perera"/>
+              </div>
+              <div>
+                <Account label1="H Perera"/>
+              </div>
+              <div>
+                <Account label1="I Perera"/>
+              </div>
+            </div>
+        </div>
+    )
+}        
+
+export default DiscoverMembers;
+
+        
+        
+        
